02cf48a59c0a311dcf666e89b83d9f527d4b3e04
Iaiao <err266228@gmail.com>
2020-04-10 15:45:17 +0300

    20w15a

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 // пока что не настраиваемый
-pub const SUPPORTED_VERSIONS  : [i32; 1]   = [710];
+pub const SUPPORTED_VERSIONS  : [i32; 1]   = [711];
 pub const MOTD                : &str       = "MOTD";
-pub const VERSION             : &str       = "20w14a";
+pub const VERSION             : &str       = "20w15a";
 pub const MAX_PLAYERS         : usize      = 228;
 pub const VIEW_DISTANCE       : u8         = 8;
